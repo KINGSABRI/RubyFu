ca86d2014b1fb4c4a3ffe48eb6d9ca5a73b9249e
Abdullah Mussad <abdullahmussad.eg@gmail.com>
2016-12-21 14:52:22 +0200

    Update header.js

--- styles/header.js
+++ styles/header.js
@@ -4,6 +4,21 @@ $(document).ready(
     $(".book-body .body-inner").niceScroll({cursorcolor:"#b21818"});
     $(".book-summary").niceScroll({cursorcolor:"#b21818"});
   }
+  
+  var face_style = {
+  "position": "fixed",
+  "top": "30%",
+  "right":"0",
+  "color":"#FFF",
+  "background":"#3b5998",
+  "width":"45px",
+  "height":"45px",
+  "text-align":"center",
+  "padding":"2px",
+  "font-size":"23px",
+  "border-radius":"5px 0px 0px 5px"
+  };
+  $(".fa-facebook").parent().css(face_style);
 
 );
 

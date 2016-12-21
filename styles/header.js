6d62d7b8920b18ac2f19e2aa6f8f47610233007b
Abdullah Mussad <abdullahmussad.eg@gmail.com>
2016-12-21 16:35:34 +0200

    Update header.js

--- styles/header.js
+++ styles/header.js
@@ -1,7 +1,8 @@
-$(document).ready( function () {
-  
-   
- $(".fa-facebook").parent().css({ "position": "fixed",
+
+window.onload = function(){
+ $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" class="logo_rubyfu"></a></div></div></div></div>');
+
+  $(".fa-facebook").parent().css({ "position": "fixed",
                                   "top": "30%",
                                   "right":"-3px",
                                   "color":"#FFF",
@@ -32,11 +33,7 @@ $(document).ready( function () {
 
     $(".book-body .body-inner").niceScroll({cursorcolor:"#b21818"});
     $(".book-summary").niceScroll({cursorcolor:"#b21818"});
-});
-
-window.onload = function(){
- $( "body" ).prepend('<div id="header" class="header absolute"><div class="wrap"><div class="clearfix" style="width:100%;"><div id="logo"><a href="https://rubyfu.net/"><img src="http://rubyfu.net/content/images/other/logo.png" class="logo_rubyfu"></a></div></div></div></div>');
-
+  
 $('.fa-spin').nextAll().remove()
 };
 
